0346c16f3757cde16af339b15a2a3fe52d4a9e18
Rahul Raturi <rahulraturi84@gmail.com>
2026-07-28 21:39:48 +0530

    added Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,12 @@ import Home from './page/Home';
 import Login from './page/Login';
 import Register from './page/Register';
 import History from './page/History';
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
-    <div>
+    <div className="flex">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
