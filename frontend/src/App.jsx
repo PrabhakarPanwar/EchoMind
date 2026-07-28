4f7d820534254387fcf7fc0304f19c27d0d71536
Prabhakar <rajapanwar808@gmail.com>
2026-07-28 10:38:57 +0530

    added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './page/Home';
+import Login from './page/Login';
+import Register from './page/Register';
+import History from './page/History';
 
 function App() {
   return (
-    <div >App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<History />} />
+      </Routes>
+    </div>
   )
 }
 
