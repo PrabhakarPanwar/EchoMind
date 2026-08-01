93851978083cf270e59fab6e03b7161bf49995dc
Prabhakar <rajapanwar808@gmail.com>
2026-08-01 23:53:10 +0530

    improve timeout

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
+const controller = new AbortController();
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  timeout: 10000,
+  timeout: 60000,
   withCredentials: true,
+  signal: controller.signal,
 });
 
 export default axiosInstance;
