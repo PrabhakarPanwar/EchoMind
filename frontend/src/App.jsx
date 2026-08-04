57c39f9a64ba4bd94813977ae27e4895fa8fd6a5
Rahul Raturi <rahulraturi84@gmail.com>
2026-08-04 11:05:58 +0530

    resolve conflict

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,11 +26,7 @@ function App() {
         <Route path="/community" element={<Community />} />
         <Route path="/register" element={<Register />} />
         <Route path="/history" element={<History />} />
-<<<<<<< HEAD
       
-=======
-        <Route path="/credits" element={<Credits />} />
->>>>>>> 75c7768c613e541d857fada5ca93d8cbb4787fe0
       </Routes>
       <ToastContainer />
     </div>
